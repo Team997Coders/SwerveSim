f22ad92aa290af7464d5007711ac083211540d74
BuildTools <hunter.barclay18@gmail.com>
2019-05-28 23:00:29 -0700

    Added direction markers. Tried to make the speed a bit more realistic. Repositioned the camera. Added a quit button and made a few builds

--- Assets/Scripts/BotController.cs
+++ Assets/Scripts/BotController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // 2 1
 // 3 4
@@ -9,7 +10,11 @@ public class BotController : MonoBehaviour {
 
   private Rigidbody rb;
 
+  public float speed, rotSpeed;
+
   public ModuleController mod1, mod2, mod3, mod4;
+
+  public Text speedomT;
 
   public void Start() {
     rb = GetComponent<Rigidbody>();
@@ -24,14 +29,18 @@ public class BotController : MonoBehaviour {
 
     holonomicMixer(f, s, r);
 
+    float modifier = getMod(Mathf.Atan2(f, s));
+
     float angle = getAngle() / 180 * Mathf.PI;
     float temp = f * Mathf.Cos(angle) + s * Mathf.Sin(angle);
     s = -f * Mathf.Sin(angle) + s * Mathf.Cos(angle);
     f = temp;
 
-    Vector3 translate = (new Vector3(s, 0, f).normalized) * 0.3f;
+    speedomT.text = "Speed Mod: " + modifier;
+
+    Vector3 translate = (new Vector3(s, 0, f).normalized) * speed;
     transform.Translate(translate);
-    transform.Rotate(new Vector3(0, r * 5, 0));
+    transform.Rotate(new Vector3(0, r * rotSpeed, 0));
   }
 
   public float getAngle() {
@@ -101,6 +110,28 @@ public class BotController : MonoBehaviour {
     mod2.setSpeed(speeds[1] * factor);
     mod3.setSpeed(speeds[2] * factor);
     mod4.setSpeed(speeds[3] * factor);
+  }
+
+  public float getMod(float aimedAngle) {
+    float best = 0;
+
+    if (mod1.speed * Mathf.Cos(mod1.angle - aimedAngle) > best) {
+      best = mod1.speed * Mathf.Cos(mod1.angle - aimedAngle);
+    }
+
+    if (mod2.speed * Mathf.Cos(mod2.angle - aimedAngle) > best) {
+      best = mod2.speed * Mathf.Cos(mod2.angle - aimedAngle);
+    }
+
+    if (mod3.speed * Mathf.Cos(mod3.angle - aimedAngle) > best) {
+      best = mod3.speed * Mathf.Cos(mod3.angle - aimedAngle);
+    }
+
+    if (mod4.speed * Mathf.Cos(mod4.angle - aimedAngle) > best) {
+      best = mod4.speed * Mathf.Cos(mod4.angle - aimedAngle);
+    }
+
+    return best;
   }
 
 }
---
+++ Assets/Scripts/CameraMan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMan : MonoBehaviour {
+
+  public Transform follow;
+
+  private Vector3 offset;
+
+  public void Start() {
+    offset = transform.position - follow.position;
+  }
+
+  public void FixedUpdate() {
+    transform.position = offset + follow.position;
+  }
+
+  public void Yeet() {
+    Application.Quit();
+  }
+
+}
