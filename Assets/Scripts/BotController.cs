8b76c7a5b0eaf66c2911d4805de106108279b971
KyroVibe <hunter.barclay18@gmail.com>
2019-09-01 03:54:53 -0700

    Added some stuff

--- Assets/Scripts/BotController.cs
+++ Assets/Scripts/BotController.cs
@@ -6,132 +6,154 @@ using UnityEngine.UI;
 // 2 1
 // 3 4
 
-public class BotController : MonoBehaviour {
+public class BotController : MonoBehaviour
+{
 
-  private Rigidbody rb;
+    private Rigidbody rb;
 
-  public float speed, rotSpeed;
+    public Material red, norm;
 
-  public ModuleController mod1, mod2, mod3, mod4;
+    public float speed, rotSpeed;
 
-  public Text speedomT;
+    public ModuleController mod1, mod2, mod3, mod4;
 
-  public void Start() {
-    rb = GetComponent<Rigidbody>();
-  }
+    public Text speedomT;
 
-  public void FixedUpdate() {
-    float f = Input.GetAxis("Vertical");
-    float s = Input.GetAxis("Horizontal");
-    float r = -Input.GetAxis("Vertical2");
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-    print(f + " " + s + " " + r);
+    public void FixedUpdate()
+    {
+        float f = Input.GetAxisRaw("Vertical");
+        float s = Input.GetAxisRaw("Horizontal");
+        float r = -Input.GetAxisRaw("Vertical2");
 
-    holonomicMixer(f, s, r);
+        print(f + " " + s + " " + r);
 
-    float modifier = getMod(Mathf.Atan2(f, s));
+        holonomicMixer(f, s, r);
 
-    float angle = getAngle() / 180 * Mathf.PI;
-    float temp = f * Mathf.Cos(angle) + s * Mathf.Sin(angle);
-    s = -f * Mathf.Sin(angle) + s * Mathf.Cos(angle);
-    f = temp;
+        float modifier = getMod(Mathf.Atan2(f, s));
 
-    speedomT.text = "Speed Mod: " + modifier;
+        float angle = getAngle() / 180 * Mathf.PI;
+        float temp = f * Mathf.Cos(angle) + s * Mathf.Sin(angle);
+        s = -f * Mathf.Sin(angle) + s * Mathf.Cos(angle);
+        f = temp;
 
-    Vector3 translate = (new Vector3(s, 0, f).normalized) * speed;
-    transform.Translate(translate);
-    transform.Rotate(new Vector3(0, r * rotSpeed, 0));
-  }
+        speedomT.text = "Speed Mod: " + modifier;
 
-  public float getAngle() {
-    return transform.rotation.eulerAngles.y;
-  }
+        Vector3 translate = (new Vector3(s, 0, f).normalized) * speed;
+        transform.Translate(translate);
+        transform.Rotate(new Vector3(0, r * rotSpeed, 0));
+    }
 
-  public void holonomicMixer(float forward, float strafe, float rotation) {
+    public float getAngle()
+    {
+        return transform.rotation.eulerAngles.y;
+    }
 
-    float angle = getAngle() / 180 * Mathf.PI;
-    float temp = forward * Mathf.Cos(angle) + strafe * Mathf.Sin(angle);
-    strafe = -forward * Mathf.Sin(angle) + strafe * Mathf.Cos(angle);
-    forward = temp;
+    public void holonomicMixer(float forward, float strafe, float rotation)
+    {
 
-    float L = 1f; // WheelBase
-    float W = 1f; // TrackWidth
-    float R = Mathf.Sqrt((L * L) + (W * W));
+        float angle = getAngle() / 180 * Mathf.PI;
+        float temp = forward * Mathf.Cos(angle) + strafe * Mathf.Sin(angle);
+        strafe = -forward * Mathf.Sin(angle) + strafe * Mathf.Cos(angle);
+        forward = temp;
 
-    float A = strafe - rotation * (L / R);
-    float B = strafe + rotation * (L / R);
-    float C = forward - rotation * (W / R);
-    float D = forward + rotation * (W / R);
+        float L = 3f; // WheelBase
+        float W = 2.5f; // TrackWidth
+        float R = Mathf.Sqrt((L * L) + (W * W));
 
-    print(A + ", " + B + ", " + C + ", " + D);
+        float A = strafe - rotation * (L / R);
+        float B = strafe + rotation * (L / R);
+        float C = forward - rotation * (W / R);
+        float D = forward + rotation * (W / R);
 
-    float[] speeds = new float[] {
+        print(A + ", " + B + ", " + C + ", " + D);
+
+        float[] speeds = new float[] {
       Mathf.Sqrt((B * B) + (C * C)),
       Mathf.Sqrt((B * B) + (D * D)),
       Mathf.Sqrt((A * A) + (D * D)),
       Mathf.Sqrt((A * A) + (C * C))
     };
 
-    float[] angles = new float[] {
+        float[] angles = new float[] {
       Mathf.Atan2(B, C) * 180 / Mathf.PI,
       Mathf.Atan2(B, D) * 180 / Mathf.PI,
       Mathf.Atan2(A, D) * 180 / Mathf.PI,
       Mathf.Atan2(A, C) * 180 / Mathf.PI
     };
 
-    float max = 1;
+        float max = 1;
 
-    if (speeds[0] != 0)
+        if (speeds[0] != 0)
+        {
+            max = speeds[0];
+        }
+
+        for (int i = 1; i < speeds.Length; i++)
+        {
+            if (max < speeds[i])
+            {
+                max = speeds[i];
+            }
+        }
+
+        float factor = 1 / max;
+
+        mod1.setAngle(angles[0]);
+        mod2.setAngle(angles[1]);
+        mod3.setAngle(angles[2]);
+        mod4.setAngle(angles[3]);
+
+        print(speeds[0]);
+        print(speeds[1]);
+        print(speeds[2]);
+        print(speeds[3]);
+
+        mod1.setSpeed(speeds[0] * factor);
+        mod2.setSpeed(speeds[1] * factor);
+        mod3.setSpeed(speeds[2] * factor);
+        mod4.setSpeed(speeds[3] * factor);
+    }
+
+    public float getMod(float aimedAngle)
     {
-      max = speeds[0];
+        float best = 0;
+
+        if (mod1.speed * Mathf.Cos(mod1.angle - aimedAngle) > best)
+        {
+            best = mod1.speed * Mathf.Cos(mod1.angle - aimedAngle);
+        }
+
+        if (mod2.speed * Mathf.Cos(mod2.angle - aimedAngle) > best)
+        {
+            best = mod2.speed * Mathf.Cos(mod2.angle - aimedAngle);
+        }
+
+        if (mod3.speed * Mathf.Cos(mod3.angle - aimedAngle) > best)
+        {
+            best = mod3.speed * Mathf.Cos(mod3.angle - aimedAngle);
+        }
+
+        if (mod4.speed * Mathf.Cos(mod4.angle - aimedAngle) > best)
+        {
+            best = mod4.speed * Mathf.Cos(mod4.angle - aimedAngle);
+        }
+
+        return best;
     }
 
-    for (int i = 1; i < speeds.Length; i++)
+    private void OnTriggerEnter(Collider other)
     {
-      if (max < speeds[i])
-      {
-        max = speeds[i];
-      }
+        other.GetComponent<Renderer>().material = red;
     }
 
-    float factor = 1 / max;
-
-    mod1.setAngle(angles[0]);
-    mod2.setAngle(angles[1]);
-    mod3.setAngle(angles[2]);
-    mod4.setAngle(angles[3]);
-
-    print(speeds[0]);
-    print(speeds[1]);
-    print(speeds[2]);
-    print(speeds[3]);
-
-    mod1.setSpeed(speeds[0] * factor);
-    mod2.setSpeed(speeds[1] * factor);
-    mod3.setSpeed(speeds[2] * factor);
-    mod4.setSpeed(speeds[3] * factor);
-  }
-
-  public float getMod(float aimedAngle) {
-    float best = 0;
-
-    if (mod1.speed * Mathf.Cos(mod1.angle - aimedAngle) > best) {
-      best = mod1.speed * Mathf.Cos(mod1.angle - aimedAngle);
+    private void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<Renderer>().material = norm;
     }
-
-    if (mod2.speed * Mathf.Cos(mod2.angle - aimedAngle) > best) {
-      best = mod2.speed * Mathf.Cos(mod2.angle - aimedAngle);
-    }
-
-    if (mod3.speed * Mathf.Cos(mod3.angle - aimedAngle) > best) {
-      best = mod3.speed * Mathf.Cos(mod3.angle - aimedAngle);
-    }
-
-    if (mod4.speed * Mathf.Cos(mod4.angle - aimedAngle) > best) {
-      best = mod4.speed * Mathf.Cos(mod4.angle - aimedAngle);
-    }
-
-    return best;
-  }
 
 }
